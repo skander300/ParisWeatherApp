4c86741f554c84ec9e429ef86ee31348f6160cb1
Skander Fathallah <skanderfathallah07@gmail.com>
2020-02-02 18:17:10 +0100

    * Feature : Forecast Details
    - Finish implementing Forecast details screen

--- ParisWeather/ParisWeather/Content/Details/View/ForecastDetailsViewController.swift
+++ ParisWeather/ParisWeather/Content/Details/View/ForecastDetailsViewController.swift
@@ -16,12 +16,11 @@ class ForecastDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupViews()
-        populate()
     }
     
     func setupViews() {
+        self.title = "Forecast details"
         let nib = UINib(nibName: "ForecastDetailsTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "ForecastDetailsTableViewCell")
         tableView.rowHeight = UITableView.automaticDimension
@@ -37,10 +36,6 @@ class ForecastDetailsViewController: UIViewController {
             tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
         }
     }
-    
-    func populate() {
-        // title = viewModel.date
-    }
 }
 
 
@@ -50,35 +45,14 @@ extension ForecastDetailsViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "ForecastCell")
-        if !(cell != nil) {
-            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "ForecastCell")
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ForecastDetailsTableViewCell") as? ForecastDetailsTableViewCell else {
+            return UITableViewCell()
         }
-        switch indexPath.row {
-        case 0:
-            cell?.textLabel?.text = "Sunrise"
-            cell?.detailTextLabel?.text = viewModel.sunrise
-        case 1:
-            cell?.textLabel?.text = "Sunset"
-            cell?.detailTextLabel?.text = viewModel.sunset
-        case 2:
-            cell?.textLabel?.text = "Chance of rain"
-            cell?.detailTextLabel?.text = viewModel.chanceOfRain
-        case 3:
-            cell?.textLabel?.text = "Wind"
-            cell?.detailTextLabel?.text = viewModel.wind
-        case 4:
-            cell?.textLabel?.text = "Humidity"
-            cell?.detailTextLabel?.text = viewModel.humidity
-        case 5:
-            cell?.textLabel?.text = "Feels like"
-            cell?.detailTextLabel?.text = viewModel.feelsLike
-        case 6:
-            cell?.textLabel?.text = "Pressure"
-            cell?.detailTextLabel?.text = viewModel.pressure
-        default: break
-        }
-        return cell!
+        
+        cell.row = indexPath.row
+        cell.viewModel = viewModel
+
+        return cell
     }
     
     
